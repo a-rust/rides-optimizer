6007ebd6ac8ffb07260d5eabfc1858211b0d97d0
a-rust <6aywagh8k@mozmail.com>
2023-04-25 21:15:00 -0700

    Assigned None to min distinct rides constraint in unit tests, as not implemented yet

--- tests/test_constant_times.py
+++ tests/test_constant_times.py
@@ -15,7 +15,7 @@ def test_set_ride_weights():
 
 # Test for maximizing the number of rides with a max time constraint
 def test_maximize_rides_with_max_time():
-    user_preferences = up.UserPreferences(required_rides=None, avoid_rides=None, min_distinct_rides=3, max_ride_repeats=None, max_time=20, min_total_rides=None)
+    user_preferences = up.UserPreferences(required_rides=None, avoid_rides=None, min_distinct_rides=None, max_ride_repeats=None, max_time=20, min_total_rides=None)
     park = ct.OptimizeConstant(rides, wait_times, ride_times, user_preferences)
     ride_weights = park.set_ride_weights()
     # Choosing to ride 'b' 3 times is optimal given max_time = 20
@@ -23,7 +23,7 @@ def test_maximize_rides_with_max_time():
 
 # Test for maximizing the number of rides with no max time constraint
 def test_maximize_rides_with_no_max_time():
-    user_preferences = up.UserPreferences(required_rides=None, avoid_rides=None, min_distinct_rides=3, max_ride_repeats=None, max_time=None, min_total_rides=None)
+    user_preferences = up.UserPreferences(required_rides=None, avoid_rides=None, min_distinct_rides=None, max_ride_repeats=None, max_time=None, min_total_rides=None)
     park = ct.OptimizeConstant(rides, wait_times, ride_times, user_preferences)
     ride_weights = park.set_ride_weights()
     # The solution is unbounded if no max_time_constraint, and so maximize_rides should return None
@@ -31,7 +31,7 @@ def test_maximize_rides_with_no_max_time():
 
 # Test for maximizing the number of rides with max ride repeats constraint
 def test_maximize_rides_with_max_ride_repeats():
-    user_preferences = up.UserPreferences(required_rides=None, avoid_rides=None, min_distinct_rides=3, max_ride_repeats=2, max_time=30, min_total_rides=None)
+    user_preferences = up.UserPreferences(required_rides=None, avoid_rides=None, min_distinct_rides=None, max_ride_repeats=2, max_time=30, min_total_rides=None)
     park = ct.OptimizeConstant(rides, wait_times, ride_times, user_preferences)
     ride_weights = park.set_ride_weights()
     # The solution is unbounded if no max_time_constraint, and so maximize_rides should return None
@@ -39,7 +39,7 @@ def test_maximize_rides_with_max_ride_repeats():
 
 # Test for maximizing the number of rides with no max ride repeats constraint
 def test_maximize_rides_with_no_max_ride_repeats():
-    user_preferences = up.UserPreferences(required_rides=None, avoid_rides=None, min_distinct_rides=3, max_ride_repeats=None, max_time=30, min_total_rides=None)
+    user_preferences = up.UserPreferences(required_rides=None, avoid_rides=None, min_distinct_rides=None, max_ride_repeats=None, max_time=30, min_total_rides=None)
     park = ct.OptimizeConstant(rides, wait_times, ride_times, user_preferences)
     ride_weights = park.set_ride_weights()
     # If no max ride repeats, all of the space will be go to 'b', as the weight of 'b' is less than the weights of 'a' and 'c'
@@ -47,7 +47,7 @@ def test_maximize_rides_with_no_max_ride_repeats():
 
 # Test for maximizing the number of rides with required rides constraint
 def test_maximize_rides_with_required_rides():
-    user_preferences = up.UserPreferences(required_rides=['a'], avoid_rides=None, min_distinct_rides=3, max_ride_repeats=None, max_time=30, min_total_rides=None)
+    user_preferences = up.UserPreferences(required_rides=['a'], avoid_rides=None, min_distinct_rides=None, max_ride_repeats=None, max_time=30, min_total_rides=None)
     park = ct.OptimizeConstant(rides, wait_times, ride_times, user_preferences)
     ride_weights = park.set_ride_weights()
     # Requiring to go on ride 'a' at least once will assign 1 to 'a', and utilize the rest of the space with 'b', as the weight of 'b' is less than the weights of 'a' and 'c'
@@ -55,7 +55,7 @@ def test_maximize_rides_with_required_rides():
 
 # Test for maximizing the number of rides with avoid rides constraint
 def test_maximize_rides_with_avoid_rides():
-    user_preferences = up.UserPreferences(required_rides=None, avoid_rides=['b'], min_distinct_rides=3, max_ride_repeats=None, max_time=30, min_total_rides=None)
+    user_preferences = up.UserPreferences(required_rides=None, avoid_rides=['b'], min_distinct_rides=None, max_ride_repeats=None, max_time=30, min_total_rides=None)
     park = ct.OptimizeConstant(rides, wait_times, ride_times, user_preferences)
     ride_weights = park.set_ride_weights()
     # Avoiding 'b' entirely will result in utilizing 'a' for all of the space, as the weight of 'a' is less than the weight of 'c'
@@ -63,7 +63,7 @@ def test_maximize_rides_with_avoid_rides():
     
 # Test for maximizing the number of rides with no required rides or avoid rides constraints
 def test_maximize_rides_with_no_required_or_avoid_rides():
-    user_preferences = up.UserPreferences(required_rides=None, avoid_rides=None, min_distinct_rides=3, max_ride_repeats=None, max_time=30, min_total_rides=None)
+    user_preferences = up.UserPreferences(required_rides=None, avoid_rides=None, min_distinct_rides=None, max_ride_repeats=None, max_time=30, min_total_rides=None)
     park = ct.OptimizeConstant(rides, wait_times, ride_times, user_preferences)
     ride_weights = park.set_ride_weights()
     # Not specifying any required rides will result in utilizing all of the space with 'b', as the weight of 'b' is less than the weights of 'a' and 'c'
@@ -71,7 +71,7 @@ def test_maximize_rides_with_no_required_or_avoid_rides():
     
 # Test for enforcing that a user cannot both require and avoid the same ride
 def test_maximize_rides_with_same_required_avoid_rides():
-    user_preferences = up.UserPreferences(required_rides=['a'], avoid_rides=['a'], min_distinct_rides=3, max_ride_repeats=None, max_time=30, min_total_rides=None)
+    user_preferences = up.UserPreferences(required_rides=['a'], avoid_rides=['a'], min_distinct_rides=None, max_ride_repeats=None, max_time=30, min_total_rides=None)
     park = ct.OptimizeConstant(rides, wait_times, ride_times, user_preferences)
     ride_weights = park.set_ride_weights()
     # Cannot require and avoid 'a' at the same time
