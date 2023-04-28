9848dd403bd5f13c6b2dc773ec29bed9c85fd7ea
a-rust <6aywagh8k@mozmail.com>
2023-04-28 13:48:35 -0700

    Add experimental data editor with random times

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 import optimization.user_preferences as up
 import optimization.constant_times as ct
 
 
 def define_problem():
     st.markdown("<h1 style='text-align: center;'>Demo</h1", unsafe_allow_html=True)
+
     time_assumption = st.selectbox("Please select the time assumptions to be used", ("Constant", "Dynamic"))
+
     optimization_problem = st.selectbox("Please choose which optimization problem you'd like to solve", ("Maximize Rides", "Minimize Time"))
+
     if optimization_problem not in st.session_state:
         st.session_state.optimization_problem = optimization_problem
+        
+    ride_data_col1, ride_data_col2 = st.columns((1, 1))
+    random_rides_data(ride_data_col1, ride_data_col2)
+
+def random_rides_data(ride_data_col1, ride_data_col2):
+    rides_col1 = [f'Ride_{i}' for i in range(1, 6)]
+    rides_col2 = np.random.randint(low=0, high=10, size=5)
+    rides_col3 = np.random.randint(low=0, high=10, size=5)
+
+    rides = pd.DataFrame({"Rides": rides_col1, "Wait Times": rides_col2, "Ride Times": rides_col3})
+
+    if "rides" not in st.session_state:
+        st.session_state.rides = rides
+
+    ride_data_col1.markdown("<h2 style='text-align: center;'>Rides</h2", unsafe_allow_html=True)
+
+    experimental_rides_df = ride_data_col1.experimental_data_editor(st.session_state.rides, num_rows="dynamic")
+
+    if experimental_rides_df not in st.session_state:
+        st.session_state.experimental_rides_df = experimental_rides_df
 
 define_problem()
