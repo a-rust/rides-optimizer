bc08f1ef9b67628f7271bae026670bd0b039f679
a-rust <6aywagh8k@mozmail.com>
2023-04-28 14:51:02 -0700

    Fix randomized optional constraints to pull from experimental df

--- app.py
+++ app.py
@@ -68,16 +68,16 @@ def random_constraints_data(ride_data_col1, ride_data_col2):
 
     ride_data_col2.markdown("<h2 style='text-align: center;'>Optional Constraints</h2", unsafe_allow_html=True)
 
-    required_rides = ride_data_col2.multiselect("Required Rides",  options=[i for i in st.session_state.rides.Rides], default=random.choice(st.session_state.rides.Rides))
+    required_rides = ride_data_col2.multiselect("Required Rides",  options=[i for i in st.session_state.rides.Rides], default=random.choice(st.session_state.experimental_rides_df.Rides))
     if required_rides not in st.session_state:
         st.session_state.required_rides = required_rides
-    avoid_rides = ride_data_col2.multiselect("Avoid Rides",  options=[i for i in st.session_state.rides.Rides], default=random.choice(st.session_state.rides.Rides))
+    avoid_rides = ride_data_col2.multiselect("Avoid Rides",  options=[i for i in st.session_state.rides.Rides], default=random.choice(st.session_state.experimental_rides_df.Rides))
     if avoid_rides not in st.session_state:
         st.session_state.avoid_rides = avoid_rides
-    min_distinct_rides_slider = ride_data_col2.slider("Minimum Distinct Rides", value=random.randint(0, len(st.session_state.rides)))
+    min_distinct_rides_slider = ride_data_col2.slider("Minimum Distinct Rides", value=random.randint(0, len(st.session_state.experimental_rides_df.Rides)))
     if min_distinct_rides_slider not in st.session_state:
         st.session_state.min_distinct_rides_slider = min_distinct_rides_slider
-    max_ride_repeats_slider = ride_data_col2.slider("Maximum Ride Repeats", value=random.randint(0, len(st.session_state.rides)))
+    max_ride_repeats_slider = ride_data_col2.slider("Maximum Ride Repeats", value=random.randint(0, len(st.session_state.experimental_rides_df.Rides)))
     if max_ride_repeats_slider not in st.session_state:
         st.session_state.max_ride_repeats_slider = max_ride_repeats_slider
 
