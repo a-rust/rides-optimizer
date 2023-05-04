a76721480662fd2760821983b3b20d614ea5b603
a-rust <6aywagh8k@mozmail.com>
2023-05-03 19:39:32 -0700

    Add optional constraints in dynamic setting

--- streamlit-app/demo_dynamic.py
+++ streamlit-app/demo_dynamic.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import random
+import helper
 import optimization.user_preferences as up
 import optimization.dynamic_times as dt
 
@@ -21,11 +22,16 @@ def main():
             del st.session_state.rand_max_time_slider_dynamic
         if st.session_state.optimization_problem == "Minimize Time":
             del st.session_state.rand_min_total_rides_slider_dynamic
-        
+        del st.session_state.rand_required_rides_dynamic
+        del st.session_state.rand_avoid_rides_dynamic 
+        del st.session_state.rand_min_distinct_rides_dynamic
+        del st.session_state.rand_max_ride_repeats_dynamic
+            
     ride_data_col1, result_col2 = st.columns((1, 1))
     granularity = demo_granularity(ride_data_col1)
     rides_dynamic = demo_rides(granularity, ride_data_col1)
     required_constraints = random_required_constraints_data(rides_dynamic)
+    user_preferences = random_optional_constraints_data(rides_dynamic, required_constraints)
 
 def demo_granularity(ride_data_col1):
 
@@ -76,5 +82,52 @@ def random_required_constraints_data(rides_dynamic):
         min_total_rides_slider = st.sidebar.slider("Minimum Number of Total Rides Constraint", max_value=5*len(rides_dynamic.Rides), value=st.session_state.rand_min_total_rides_slider_dynamic, help="What is the minimum total number of rides you'd like to go on?")
     else:
         min_total_rides_slider = None
-        
+
     return [max_time_slider, min_total_rides_slider]
+
+def random_optional_constraints_data(rides_dynamic, required_constraints):
+    st.sidebar.markdown("<h2 style='text-align: center;'>Optional Constraints</h2", unsafe_allow_html=True, help="These constraints are optional, but make the optimization much more interesting")
+    rand_required_rides_dynamic = random.choice(rides_dynamic.Rides)
+    if "rand_required_rides_dynamic" not in st.session_state:
+        st.session_state.rand_required_rides_dynamic = rand_required_rides_dynamic
+    required_rides = st.sidebar.multiselect("Required Rides",  options=[i for i in rides_dynamic.Rides], default=st.session_state.rand_required_rides_dynamic, help="Which rides would you like to go on at least once?")
+
+    # Prevent random avoid ride to be distinct from random required ride
+    rand_avoid_rides_dynamic = random.choice(list(set(rides_dynamic.Rides).difference(set(required_rides))))
+    if "rand_avoid_rides_dynamic" not in st.session_state:
+        st.session_state.rand_avoid_rides_dynamic = rand_avoid_rides_dynamic
+    avoid_rides = st.sidebar.multiselect("Avoid Rides",  options=[i for i in rides_dynamic.Rides], default=st.session_state.rand_avoid_rides_dynamic, help="Which rides would you like to avoid entirely?")
+
+    # Prevent user from requiring and avoiding the same ride(s)
+    helper.require_avoid_contradiction(required_rides, avoid_rides)
+
+    # The rand_min_distinct_rides should not be larger than either (# of rides - # of avoided rides) or the min total rides (in the case of a minimization problem)
+    if required_constraints[1] != None:
+        rand_min_distinct_rides_dynamic = random.randint(0, min(len(rides_dynamic.Rides) - len(avoid_rides), required_constraints[1]))
+    else: 
+        rand_min_distinct_rides_dynamic = random.randint(1, len(rides_dynamic.Rides) - len(avoid_rides))
+    if "rand_min_distinct_rides_dynamic" not in st.session_state:
+        st.session_state.rand_min_distinct_rides_dynamic = rand_min_distinct_rides_dynamic
+    min_distinct_rides_slider = st.sidebar.slider("Minimum Distinct Rides", min_value=1, max_value=len(rides_dynamic.Rides), value=st.session_state.rand_min_distinct_rides_dynamic, help="What is the minimum number of distinct rides you'd like to go on?")
+
+    helper.min_distinct_rides_contradiction(min_distinct_rides_slider, len(rides_dynamic.Rides), len(avoid_rides))
+
+    rand_max_ride_repeats_dynamic = random.randint(1, len(rides_dynamic.Rides))
+    if "rand_max_ride_repeats_dynamic" not in st.session_state:
+        st.session_state.rand_max_ride_repeats_dynamic = rand_max_ride_repeats_dynamic
+    max_ride_repeats_slider = st.sidebar.slider("Maximum Ride Repeats", min_value=1, value=st.session_state.rand_max_ride_repeats_dynamic, help="What is the maximum number of times you'd like to ride any single ride?")
+
+    if st.session_state.optimization_problem == "Minimize Time":
+        helper.max_ride_repeats_contradiction(required_constraints[1], max_ride_repeats_slider, len(rides_dynamic.Rides))
+
+    user_preferences=up.UserPreferences(
+        required_rides,
+        avoid_rides,
+        min_distinct_rides_slider,
+        max_ride_repeats_slider,
+        {1: required_constraints[0]},
+        required_constraints[1]
+        ) 
+
+    user_preferences.convert_empty_data_types()
+    return user_preferences
