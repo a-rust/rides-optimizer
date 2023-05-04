17061cbc43e2e52e120619caa7c97747d42c3add
a-rust <6aywagh8k@mozmail.com>
2023-05-03 18:14:55 -0700

    Add required constraints in dynamic setting

--- streamlit-app/demo_dynamic.py
+++ streamlit-app/demo_dynamic.py
@@ -17,10 +17,15 @@ def main():
         del st.session_state.rand_granularity
         del st.session_state.rand_times
         del st.session_state.rides_dynamic
+        if st.session_state.optimization_problem == "Maximize Rides":
+            del st.session_state.rand_max_time_slider_dynamic
+        if st.session_state.optimization_problem == "Minimize Time":
+            del st.session_state.rand_min_total_rides_slider_dynamic
         
     ride_data_col1, result_col2 = st.columns((1, 1))
     granularity = demo_granularity(ride_data_col1)
-    rand_rides_dynamic = demo_rides(granularity, ride_data_col1)
+    rides_dynamic = demo_rides(granularity, ride_data_col1)
+    required_constraints = random_required_constraints_data(rides_dynamic)
 
 def demo_granularity(ride_data_col1):
 
@@ -53,3 +58,23 @@ def demo_rides(granularity: int, ride_data_col1):
 
     experimental_rides_dynamic_df = ride_data_col1.experimental_data_editor(rides_dynamic, num_rows="dynamic")
     return experimental_rides_dynamic_df
+
+def random_required_constraints_data(rides_dynamic):
+    st.sidebar.markdown("<h2 style='text-align: center;'>Required Constraint</h2", unsafe_allow_html=True, help="This constraint must be set to have any meaningful results")
+    if st.session_state.optimization_problem == "Maximize Rides":
+        # Artificial lower bound on max time constraint to avoid infeasibility of randomly generated data
+        rand_max_time_slider_dynamic = random.randint(10*len(rides_dynamic.Rides), 300)
+        if "rand_max_time_slider_dynamic" not in st.session_state:
+            st.session_state.rand_max_time_slider_dynamic = rand_max_time_slider_dynamic
+        max_time_slider = st.sidebar.slider("Maximum Time Constraint", max_value=300, value=st.session_state.rand_max_time_slider_dynamic, help="What is the maximum total amount of time you'd like to spend waiting and riding rides?")
+    else:
+        max_time_slider = None
+    if st.session_state.optimization_problem == "Minimize Time":
+        rand_min_total_rides_slider_dynamic = random.randint(0, 10*len(rides_dynamic.Rides))
+        if "rand_min_total_rides_slider_dynamic" not in st.session_state:
+            st.session_state.rand_min_total_rides_slider_dynamic = rand_min_total_rides_slider_dynamic
+        min_total_rides_slider = st.sidebar.slider("Minimum Number of Total Rides Constraint", max_value=5*len(rides_dynamic.Rides), value=st.session_state.rand_min_total_rides_slider_dynamic, help="What is the minimum total number of rides you'd like to go on?")
+    else:
+        min_total_rides_slider = None
+        
+    return [max_time_slider, min_total_rides_slider]
