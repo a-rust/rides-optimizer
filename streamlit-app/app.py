2e35e556169aae58f3d259b608e76af64f7c1082
a-rust <6aywagh8k@mozmail.com>
2023-05-11 03:48:56 -0700

    Add ParkData class to handle API requests

--- streamlit-app/app.py
+++ streamlit-app/app.py
@@ -1,21 +1,32 @@
 import streamlit as st
 import demo_constant
 import demo_dynamic
+import park_data
 
-def define_problem():
-    park = st.sidebar.selectbox(label="Park", options=("Demo", "Empty for now"))
+
+def define_park():
+    park = st.sidebar.selectbox(label="Park", options=("Demo", "Disneyland Resort Magic Kingdom", "Disneyland Resort California Adventure", "Universal Studios"))
     if park == "Demo":
         st.markdown("<h1 style='text-align: center;'>Demo</h1", unsafe_allow_html=True)
-        time_assumption = st.selectbox("Please select the time assumptions to be used", ("Constant", "Dynamic"), help="Do you want to assume constant or dynamic wait and ride times?")
-        return time_assumption
-    else:
-        return
+    elif park == "Disneyland Resort Magic Kingdom":
+        st.markdown("<h1 style='text-align: center;'>Disneyland Resort Magic Kingdom</h1", unsafe_allow_html=True)
+    elif park == "Disneyland Resort California Adventure":
+        st.markdown("<h1 style='text-align: center;'>Disneyland Resort California Adventure</h1")
+    elif park == "Universal Studios":
+        st.markdown("<h1 style='text-align: center;'>Universal Studios</h1", unsafe_allow_html=True)
+    return park
+
+def define_time_assumption():
+    time_assumption = st.selectbox("Please select the time assumptions to be used", ("Constant", "Dynamic"), help="Do you want to assume constant or dynamic wait and ride times?")
+    return time_assumption
 
 def main():
-    time_assumption = define_problem()
-    if time_assumption == "Constant":
-        demo_constant.main()
-    elif time_assumption == "Dynamic":
-        demo_dynamic.main()
+    park = define_park()
+    time_assumption = define_time_assumption()
+    if park == "Demo":
+        if time_assumption == "Constant":
+            demo_constant.main()
+        elif time_assumption == "Dynamic":
+            demo_dynamic.main()
 
 main()
