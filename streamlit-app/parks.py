f1f767dd9fe6655cdcf2f570cbc083b85cbe0d48
a-rust <6aywagh8k@mozmail.com>
2023-05-18 18:38:14 -0700

    Add dataframe to display optimal solution for given park

--- streamlit-app/parks.py
+++ streamlit-app/parks.py
@@ -10,27 +10,33 @@ sys.path.append(path)
 import park_data
 from optimization import user_preferences as up
 from optimization import constant_times as ct
+from optimization import dynamic_times as dt
 import helper
 
 
 class OptimizePark():
-    def __init__(self, name: str, all_rides: dict, active_rides: dict, time_assumption: str) -> None:
+    def __init__(self, name: str, active_rides: dict, time_assumption: str) -> None:
         self.name = name
-        self.all_rides = all_rides
         self.active_rides = active_rides
         self.time_assumption = time_assumption
 
     def main(self):
+        global time_periods
         optimization_problem = st.selectbox("Please choose which optimization problem you'd like to solve", ("Maximize Rides", "Minimize Time"), help="Do you want to maximize the total number of rides to go on, or minimize the total amount of time spent at the park?")
         if optimization_problem not in st.session_state:
             st.session_state.optimization_problem = optimization_problem
+        col1, col2 = st.columns((1, 1))
         if self.time_assumption == "Constant":
-            rides = self.park_rides(None)
+            rides = self.park_rides(col1, None)
         elif self.time_assumption == "Dynamic":
             time_periods = self.granularity()
-            rides = self.park_rides(time_periods)
+            rides = self.park_rides(col1, time_periods)
         required_constraints = self.required_constraints(rides)
         optional_constraints = self.optional_constraints(rides, required_constraints)
+        if self.time_assumption == "Constant":
+            self.optimize(col2, rides, optional_constraints, None)
+        elif self.time_assumption == "Dynamic":
+            self.optimize(col2, rides, optional_constraints, time_periods)
 
     def granularity(self):
         st.sidebar.markdown("<h2 style='text-align: center;'>Time Updates</h2", unsafe_allow_html=True, help="Control the time changes")
@@ -42,30 +48,28 @@ class OptimizePark():
         time_periods = st.sidebar.slider("How many time periods will there be?", min_value=2, max_value=20, value=st.session_state.rand_time_periods)
         return [time_periods, frequency]
 
-    def park_rides(self, granularity: typing.Optional[list] = None):
+    def park_rides(self, col1, granularity: typing.Optional[list] = None):
         if self.time_assumption == "Constant":
             park_rides = pd.DataFrame({"Rides": list(self.active_rides.keys()), "Wait Times": list(self.active_rides.values())})
 
         elif self.time_assumption == "Dynamic":
             park_rides = pd.DataFrame({"Rides": list(self.active_rides.keys()), "Wait Times Period 1": list(self.active_rides.values())})
             for i in range(1, granularity[0]):
-                park_rides[f'Wait Times Period {i+1}'] = None
+                park_rides[f'Wait Times Period {i+1}'] = 0
         
-        st.markdown("<h2 style='text-align: center;'>Active Rides</h2",
+        col1.markdown("<h2 style='text-align: center;'>Active Rides</h2",
                             unsafe_allow_html=True, help="Feel free to add, delete, or edit the rides data")
-        experimental_rides_df = st.experimental_data_editor(park_rides, num_rows="fixed")
+        experimental_rides_df = col1.experimental_data_editor(park_rides, num_rows="fixed")
 
         return experimental_rides_df
 
     def required_constraints(self, rides):
         st.sidebar.markdown("<h2 style='text-align: center;'>Required Constraint</h2", unsafe_allow_html=True, help="This constraint must be set to have any meaningful results")
         if st.session_state.optimization_problem == "Maximize Rides":
-            rand_max_time_slider = random.randint(len(rides.Rides), 5*len(rides.Rides))
-            max_time_slider = st.sidebar.slider("Maximum Time Constraint", max_value=300, value=rand_max_time_slider, help="What is the maximum total amount of time you'd like to spend waiting and riding rides?")
+            max_time_slider = st.sidebar.slider("Maximum Time Constraint", max_value=300, help="What is the maximum total amount of time you'd like to spend waiting and riding rides?")
             min_total_rides_slider = None
         elif st.session_state.optimization_problem == "Minimize Time":
-            rand_min_total_rides_slider = random.randint(0, len(rides.Rides))
-            min_total_rides_slider = st.sidebar.slider("Minimum Number of Total Rides Constraint", max_value=5*len(rides.Rides), value=rand_min_total_rides_slider, help="What is the minimum total number of rides you'd like to go on?")
+            min_total_rides_slider = st.sidebar.slider("Minimum Number of Total Rides Constraint", max_value=5*len(rides.Rides), help="What is the minimum total number of rides you'd like to go on?")
             max_time_slider = None
         return [max_time_slider, min_total_rides_slider]
 
@@ -92,3 +96,67 @@ class OptimizePark():
             required_constraints[0],
             required_constraints[1]
             )
+        
+        user_preferences.convert_empty_data_types()
+        return user_preferences
+
+    def optimize(self, col2, rides, user_preferences, granularity):
+        global optimize_data
+        if self.time_assumption == "Constant":
+            optimize_data = ct.OptimizeConstant(
+                all_rides=rides.iloc[:, 0].tolist(),
+                wait_times=rides.iloc[:, 1].tolist(),
+                user_preferences=user_preferences 
+                )
+        elif self.time_assumption == "Dynamic":
+            wait_times_lists = []
+            for i in range(1, granularity[0]+1):
+                wait_times_lists.append(rides.iloc[:, i].tolist(),)
+
+            wait_times = {}
+            for i in range(1, granularity[0]+1):
+                wait_times.update({i: wait_times_lists[i-1]})
+
+            optimize_data = dt.OptimizeDynamic(
+                all_rides=rides.iloc[:, 0].tolist(),
+                time_steps=granularity[0],
+                frequency=granularity[1],
+                wait_times=wait_times,
+                user_preferences=user_preferences 
+                )    
+            
+        ride_weights = optimize_data.set_ride_weights()
+
+        col2.markdown("<h2 style='text-align: center;'>Optimal Results</h2", unsafe_allow_html=True, help="The 'Values' represent the number of times to go on a given ride")
+        if st.session_state.optimization_problem == "Maximize Rides":
+            results = optimize_data.maximize_rides(ride_weights)
+        elif st.session_state.optimization_problem == "Minimize Time":
+            results = optimize_data.minimize_time(ride_weights)
+
+        if self.time_assumption == "Constant":
+            try:
+                ride_values = pd.DataFrame({"Rides": list(results.keys()), "Values": list(results.values())})
+                col2.dataframe(ride_values)
+            except:
+                st.error(body="No feasible solution. If this is a stand-alone error message, consider increasing the max time constraint")
+            
+        elif self.time_assumption == "Dynamic":
+            ride_values = pd.DataFrame({"Rides": list(rides.Rides)})
+            categorized_results = {}
+            for key, value in results.items():
+                index = key[1]
+                if index not in categorized_results:
+                    categorized_results[index] = {}
+                categorized_results[index][key] = value
+
+            # Put the categorized data into a list of dicts; one for each time period
+            time_period_results = []
+            for time_step in range(1, granularity[0]+1):
+                rides = list(categorized_results[time_step].keys())
+                values = list(categorized_results[time_step].values())
+                # Isolate the rides from the time periods
+                isolated_rides = [ride[0] for ride in rides]
+                time_period_results.append(dict(zip(isolated_rides, values)))
+            for i in range(len(time_period_results)):
+                ride_values[f'Results at Time Period {i+1}'] = list(time_period_results[i].values())
+            col2.dataframe(ride_values)
